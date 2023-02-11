7c466974d0f5fe9e851d62d50a3960b48d12fb71
Manjinder Singh <rahul.manjinder@yahoo.com>
2023-02-10 23:20:14 -0500

    first commit

---
+++ simulator/flask_api.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 10 17:18:24 2023
+
+@author: rahul
+"""
+
+import logging
+import json
+from flask import Flask, jsonify,request
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/status_value', methods=['GET']) 
+def function1():
+    response = None 
+    try:
+        my_resp ={"user_id": "ABC123",
+					"wheelchair_id": "XYZ123",
+                    "lock_status": "LOCKED"}
+        if my_resp.empty:
+            response = "Error while Sending data - error"
+        else:
+            print("Extracted")
+    except Exception as err :
+        print('Unexpected Error:', err)
+        response = "Error while sending the Status!"    
+    resp =None
+    resp = jsonify(my_resp)
+    return resp
+
+
+@app.route('/receive_resp', methods=['POST']) 
+def function2():
+    data = request.get_json()
+    print("Data:- ",data)
+    logging.info(data)
+    logging.info(data)
+    response = {
+        "message": "JSON received successfully",
+        "status_code": 200,
+        "success": True
+    }
+    return json.dumps(response)
+        
+if __name__ == '__main__':
+    print("Working dir = ", os.path.abspath(os.getcwd()))
+    app.run(host="127.0.0.1", port="5003", threaded=True)     
+            
+
