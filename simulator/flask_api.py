6a25aa7545ffd2233b216f117f003d1668c08a72
Ameya Ade <ameya_a@ameyas-mbp.home>
2023-02-20 20:35:56 -0500

    Added simulation for sockets and locking

--- simulator/flask_api.py
+++ simulator/flask_api.py
@@ -9,42 +9,85 @@ import logging
 import json
 from flask import Flask, jsonify,request
 import os
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+# # define a function to handle incoming messages
+# @sio.on('message')
+# def handle_message(sid, data):
+#     print('received message:', data)
+#     sio.emit('message', data)
+
+# # define a function to handle new connections
+# @sio.on('connect')
+# def handle_connect(sid, environ):
+#     print('connected')
+#     sio.emit('message', 'A new client has connected.')
+
+# # define a function to handle disconnections
+# @sio.on('disconnect')
+# def handle_disconnect(sid):
+#     print('disconnected')
+
+@app.route('/send', methods=['GET'])
+def send_message():
+    socketio.emit('response', {'data': 'This is a response from the server.'})
+    response = {'message': 'This is an example response.'}
+    return jsonify(response)
+
+@socketio.on('message')
+def handle_message(data):
+    # Handle incoming messages from clients
+    print('received message:', data)
 
 
 @app.route('/status_value', methods=['GET']) 
 def function1():
     response = None 
     try:
+        # print("status ----"+ status)
+        contents = ""
         my_resp ={"user_id": "ABC123",
 					"wheelchair_id": "XYZ123",
                     "lock_status": "LOCKED"}
-        if my_resp.empty:
-            response = "Error while Sending data - error"
-        else:
-            print("Extracted")
+        with open("sample.txt", "r") as file:
+            # Writing to the file
+            contents = file.read()
+            # print("file content "+ contents)
+            my_resp["lock_status"] = contents
+        # if not response:
+        #     response = "Error while Sending data - error"
+        # else:
+        #     print("Extracted")
     except Exception as err :
-        print('Unexpected Error:', err)
+        # print('Unexpected Error:', err)
         response = "Error while sending the Status!"    
-    resp =None
+    resp = None
     resp = jsonify(my_resp)
+    # print(my_resp)
     return resp
-
 
 @app.route('/receive_resp', methods=['POST']) 
 def function2():
     data = request.get_json()
-    print("Data:- ",data)
-    logging.info(data)
+    # print("Data:- ",data)
+    # logging.info(data)
     logging.info(data['status'])
     response = {
-        "status" : data['status']
+        "status" : ""
         }
+    with open("sample.txt", "w") as file:
+            # Writing to the file
+            file.write(data['status'])
+            response["status"] = data['status']
+            print(data['status'])
+    # print(response["status"])
     return json.dumps(response)
         
 if __name__ == '__main__':
     print("Working dir = ", os.path.abspath(os.getcwd()))
-    app.run(host="127.0.0.1", port="5003", threaded=True)     
+    app.run(host="127.0.0.1", port="5003", threaded=True) 
+    socketio.run(app, debug=True)    
             
-
